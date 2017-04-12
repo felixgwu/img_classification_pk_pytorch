db19c45f1619d84056431e84b50fb27a17fab0c6
Felix Wu <felixgwu@gmail.com>
2017-04-12 17:13:36 -0400

    change script

--- scripts/cifar10.sh
+++ scripts/cifar10.sh
@@ -2,12 +2,16 @@
 
 # ResNet
 # python3 main.py --arch resnet --depth 56 --save save/cifar10-resnet-56 --data cifar10 --epochs 164
-# python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56 --data cifar10+ --epochs 164
+# python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56-e164-b64 --data cifar10+ --epochs 164
+python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56-e164-b64-normalized --data cifar10+ --epochs 164 --normalized
+# python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56-e300-b128 --data cifar10+ --epochs 300 --batch-size 128
+python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56-e300-b128-normalized --data cifar10+ --epochs 300 -batch-size 128 --normalized
+python3 main.py --arch resnet --depth 56 --save save/cifar10+-resnet-56-e300-b64-normalized --data cifar10+ --epochs 300 -batch-size 64 --normalized
 # python3 main.py --arch resnet --depth 110 --save save/cifar10-resnet-110 --data cifar10 --epochs 164
 # python3 main.py --arch resnet --depth 110 --save save/cifar10+-resnet-110 --data cifar10+ --epochs 164
 
 # ResNet with Stochastic Depth
-python3 main.py --arch resnet --depth 110 --save save/cifar10+-resnet-stoch-110 --data cifar10+ --epochs 164 --death-mode linear --death-rate 0.5
+# python3 main.py --arch resnet --depth 110 --save save/cifar10+-resnet-stoch-110 --data cifar10+ --epochs 164 --death-mode linear --death-rate 0.5
 
 
 # DenseNet
