98828957ad2158e4dfcb264a808bdeb785c2015c
Felix Wu <felixgwu@gmail.com>
2017-04-22 15:38:11 -0400

    create args.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-import argparse
 import os
 import shutil
 import time
 import sys
-import glob
 
 import torch
 import torch.nn as nn
@@ -22,114 +20,12 @@ from importlib import import_module
 import config
 from dataloader import getDataloaders
 from utils import (save_checkpoint, get_optimizer)
-from train import Trainer
+from args import arg_parser, arch_resume_names
 
 try:
     from tensorboard_logger import configure, log_value
 except BaseException:
     configure = None
-
-model_names = list(map(lambda n: os.path.basename(n)[:-3],
-                       glob.glob('models/[A-Za-z]*.py')))
-
-parser = argparse.ArgumentParser(
-                description='Image classification PK main script')
-
-exp_group = parser.add_argument_group('exp', 'experiment setting')
-exp_group.add_argument('--save', default='save/default-{}'.format(time.time()),
-                       type=str, metavar='SAVE',
-                       help='path to the experiment logging directory'
-                       '(default: save/debug)')
-exp_group.add_argument('--resume', default='', type=str, metavar='PATH',
-                       help='path to latest checkpoint (default: none)')
-exp_group.add_argument('--evaluate', dest='evaluate', default='',
-                       choices=['', 'val', 'test'],
-                       help='eval mode: evaluate model on val/test set'
-                       ' (default: training mode)')
-exp_group.add_argument('-f', '--force', dest='force', action='store_true',
-                       help='force to overwrite existing save path')
-exp_group.add_argument('--print-freq', '-p', default=100, type=int,
-                       metavar='N', help='print frequency (default: 100)')
-exp_group.add_argument('--no_tensorboard', dest='tensorboard',
-                       action='store_false',
-                       help='do not use tensorboard_logger for logging')
-
-# dataset related
-data_group = parser.add_argument_group('data', 'dataset setting')
-data_group.add_argument('--data', metavar='D', default='cifar10',
-                        choices=config.datasets.keys(),
-                        help='datasets: ' +
-                        ' | '.join(config.datasets.keys()) +
-                        ' (default: cifar10)')
-data_group.add_argument('--data_root', metavar='DIR', default='data',
-                        help='path to dataset (default: data)')
-data_group.add_argument('-j', '--workers', dest='num_workers', default=4,
-                        type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-data_group.add_argument('--normalized', action='store_true',
-                        help='normalize the data into zero mean and unit std')
-
-# model arch related
-arch_group = parser.add_argument_group('arch', 'model architecture setting')
-arch_group.add_argument('--arch', '-a', metavar='ARCH', default='resnet',
-                        type=str, choices=model_names,
-                        help='model architecture: ' +
-                        ' | '.join(model_names) +
-                        ' (default: resnet)')
-arch_group.add_argument('-d', '--depth', default=56, type=int, metavar='D',
-                        help='depth (default=56)')
-arch_group.add_argument('--drop-rate', default=0.0, type=float,
-                        metavar='DROPRATE', help='dropout rate (default: 0.2)')
-arch_group.add_argument('--death-mode', default='none',
-                        choices=['none', 'linear', 'uniform'],
-                        help='death mode (default: none)')
-arch_group.add_argument('--death-rate', default=0.5, type=float,
-                        help='death rate rate (default: 0.5)')
-arch_group.add_argument('--growth-rate', default=12, type=int,
-                        metavar='GR', help='Growth rate of DenseNet'
-                        ' (1 means dot\'t use compression) (default: 0.5)')
-arch_group.add_argument('--bn-size', default=4, type=int,
-                        metavar='B', help='bottle neck ratio of DenseNet'
-                        ' (0 means dot\'t use bottle necks) (default: 4)')
-arch_group.add_argument('--compression', default=0.5, type=float,
-                        metavar='C', help='compression ratio of DenseNet'
-                        ' (1 means dot\'t use compression) (default: 0.5)')
-# used to set the argument when to resume automatically
-arch_resume_names = ['arch', 'depth', 'death_mode', 'death_rate', 'death_rate',
-                     'growth_rate', 'bn_size', 'compression']
-
-# training related
-optim_group = parser.add_argument_group('optimization', 'optimization setting')
-optim_group.add_argument('--epochs', default=164, type=int, metavar='N',
-                         help='number of total epochs to run (default 164)')
-optim_group.add_argument('--start-epoch', default=1, type=int, metavar='N',
-                         help='manual epoch number (useful on restarts)')
-optim_group.add_argument('--patience', default=0, type=int, metavar='N',
-                         help='patience for early stopping'
-                         '(0 means no early stopping)')
-optim_group.add_argument('-b', '--batch-size', default=64, type=int,
-                         metavar='N', help='mini-batch size (default: 64)')
-optim_group.add_argument('--optimizer', default='sgd',
-                         choices=['sgd', 'rmsprop', 'adam'], metavar='N',
-                         help='optimizer (default=sgd)')
-optim_group.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                         metavar='LR',
-                         help='initial learning rate (default: 0.1)')
-optim_group.add_argument('--decay_rate', default=0.1, type=float, metavar='N',
-                         help='decay rate of learning rate (default: 0.1)')
-optim_group.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                         help='momentum (default=0.9)')
-optim_group.add_argument('--no_nesterov', dest='nesterov',
-                         action='store_false',
-                         help='do not use Nesterov momentum')
-optim_group.add_argument('--alpha', default=0.99, type=float, metavar='M',
-                         help='alpha for ')
-optim_group.add_argument('--beta1', default=0.9, type=float, metavar='M',
-                         help='beta1 for Adam (default: 0.9)')
-optim_group.add_argument('--beta2', default=0.999, type=float, metavar='M',
-                         help='beta2 for Adam (default: 0.999)')
-optim_group.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                         metavar='W', help='weight decay (default: 1e-4)')
 
 
 def getModel(arch, **kargs):
@@ -149,7 +45,7 @@ def main():
     best_err1 = 100.
     best_epoch = 0
 
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
     args.config_of_data = config.datasets[args.data]
     args.num_classes = config.datasets[args.data]['num_classes']
     if configure is None:
@@ -199,11 +95,16 @@ def main():
     # define optimizer
     optimizer = get_optimizer(model, args)
 
+    Trainer = import_module(args.trainer).Trainer
     trainer = Trainer(model, criterion, optimizer, args)
 
-
     # create dataloader
-    if args.evaluate == 'val':
+    if args.evaluate == 'train':
+        train_loader, _, _ = getDataloaders(
+            splits=('train'), **vars(args))
+        trainer.test(train_loader, best_epoch)
+        return
+    elif args.evaluate == 'val':
         _, val_loader, _ = getDataloaders(
             splits=('val'), **vars(args))
         trainer.test(val_loader, best_epoch)
